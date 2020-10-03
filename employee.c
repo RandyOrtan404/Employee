5bd629aca6251422186167f5c9f28e99f2af04cc
Mukul Dhurkunde <72158336+MukulDhurkunde@users.noreply.github.com>
2020-10-03 18:05:45 +0530

    Update employee.c
    Adding name

--- employee.c
+++ employee.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
-int showdata(int id,int salary);
+int showdata(int id,int salary,char name[20]);
 int main()
 {
     int id,salary;
+    char name[20];
     printf("Enter the information about employee\n");
     printf("Enter the id of employe\n");
     scanf("%d",&id);
     printf("Enter the salary of employe\n");
     scanf("%d",&salary);
+    printf("Enter the name of employe\n");
+    scanf("%s",&name);
     printf("**** Information about employee *****.\n");
-    showdata(id,salary);
+    showdata(id,salary,name);
 
     return 0;
 }
-int showdata(int id ,int salary){
+int showdata(int id ,int salary,char name[20]){
     printf("ID of employee is %d\n",id);
     printf("Salary of employee is %d\n",salary);
+    printf("Name of the employee is %s\n",name);
 }
